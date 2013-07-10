202338835aba4759a2de2f1973c5bde90f32ea56
john doe <heroku.test@hushmail.com>
2013-07-10 18:48:02 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -1,15 +1,16 @@
 var express = require('express');
+
 var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
 var content = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString(content));
+  response.send(buffer.toString("utf-8", content));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-:
